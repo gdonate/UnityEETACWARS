3402dc9a710644a8549e2b1199b7333c7089bf2d
unknown <gabriel.donate@gmail.com>
2020-12-17 20:16:56 +0100

    Añadido Tilemap Colider en una capa

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,15 +6,24 @@ public class Player : MonoBehaviour
 {
     public float speed = 3f;
     Animator anim;
+    Rigidbody2D rb2d;
+    Vector2 mov; //Ahora es visible entre los metodos
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Detectamos el movimiento en un vector 2D
+        mov = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+        );
+        /*
         Vector3 mov = new Vector3( 
             Input.GetAxisRaw("Horizontal"),
             Input.GetAxisRaw("Vertical"),
@@ -25,8 +34,13 @@ public class Player : MonoBehaviour
             transform.position + mov,
             speed * Time.deltaTime
         );
+        */
         anim.SetFloat("movX", mov.x);
         anim.SetFloat("movY", mov.y);
         
+    }
+    void FixedUpdate(){
+        //Nos movemos en el fixed por las fisicas
+        rb2d.MovePosition(rb2d.position + mov * speed * Time.deltaTime);
     }
 }
