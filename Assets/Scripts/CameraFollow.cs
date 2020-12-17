c5ab311da5040bd414f83fccc11bb6edcabb0d06
unknown <gabriel.donate@gmail.com>
2020-12-17 22:32:08 +0100

    la camara ya sigue al personaje

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Player;
+    Vector3 vel = Vector3.zero;
+    public float Tiemsua = .15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 Pospla = Player.position;
+        Pospla.z = transform.position.z;
+
+        transform.position = Vector3.SmoothDamp(transform.position, Pospla, ref vel, Tiemsua);
+    }
+
+}
