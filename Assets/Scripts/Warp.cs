f8fb6758ad57d4ea1224b6485e8373397c935d68
unknown <gabriel.donate@gmail.com>
2020-12-19 21:12:23 +0100

    Hecho teleport entre mapa principal y biblioteca

---
+++ Assets/Scripts/Warp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour
+{
+    public GameObject Portal, Player;
+
+
+    void OnTriggerEnter2D( Collider2D collision){
+        if (collision.tag == "Player"){
+           
+           Player.transform.position = Portal.transform.GetChild(0).transform.position;
+        } 
+    }
+   
+}
