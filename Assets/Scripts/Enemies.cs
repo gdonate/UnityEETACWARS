20ac2d516b467583995ce21b6b9881e08fe0d341
unknown <gabriel.donate@gmail.com>
2021-01-02 19:47:25 +0100

    arreglado animaciones enemigo y movimiento. Falta que duerma cuando vuelve a la posición inicial

--- Assets/Scripts/Enemies.cs
+++ Assets/Scripts/Enemies.cs
@@ -21,6 +21,7 @@ public class Enemies : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
 
         initialPosition = transform.position;
+        i = transform.position;
         anim = GetComponent<Animator>();
     }
 
@@ -31,6 +32,20 @@ public class Enemies : MonoBehaviour
 
         float dist = Vector3.Distance(player.transform.position, transform.position);
         Vector3 dir = (target - transform.position).normalized;
+
+        RaycastHit2D hit = Physics2D.Raycast(
+            transform.position, 
+            player.transform.position - transform.position, 
+            visionRadius, 
+            1 << LayerMask.NameToLayer("Default") 
+                // Poner el propio Enemy en una layer distinta a Default para evitar el raycast
+                // También poner al objeto Attack y al Prefab Slash una Layer Attack 
+                // Sino los detectará como entorno y se mueve atrás al hacer ataques
+        );
+
+        
+        // Si el Raycast encuentra al jugador lo ponemos de target
+       
         if (dist < visionRadius)
         {
             target = player.transform.position;
@@ -38,13 +53,11 @@ public class Enemies : MonoBehaviour
             anim.SetFloat("movX", dir.x);
             anim.SetFloat("movY", dir.y);
             anim.SetBool("walking", true);
+            
         } 
-        if (target != initialPosition && dist < attackRadius){
-            // Aquí le atacaríamos, pero por ahora simplemente cambiamos la animación
-            anim.SetFloat("movX", dir.x);
-            anim.SetFloat("movY", dir.y);
-            anim.Play("Enemy_Walk", -1, 0);  // Congela la animación de andar
-        }
+        
+        
+        
 
         float fixedSpeed = speed*Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, target, fixedSpeed);
