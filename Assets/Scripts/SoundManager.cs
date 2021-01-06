bc5cc79f8757f128aad47a976244a130f6e759fa
unknown <gabriel.donate@gmail.com>
2021-01-06 12:47:46 +0100

    añadido codigo musica

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,18 +1,52 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public AudioSource musicSource;
+    public AudioSource sfxSource;
+
+    public float lowPitchRange = 0.95f;
+    public float highPitchRange = 1.05f;
+
+    public static SoundManager instance;
+
+
+//Singleton//
+    private void Awake()
     {
-        
+        if (SoundManager.instance == null)
+        {
+            SoundManager.instance = this;
+        }
+        else
+        {
+            if (SoundManager.instance != this)
+            {
+                Destroy(gameObject);
+            }
+
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaySingle (AudiClip clip)
     {
-        
+        sfxSource.clip = clip;
+        sfxSource.Play();
+
     }
+
+    public void RandomizeSfx(params AudiClip [] clips)
+    {
+        int randomIndex = Random.Range (0, clips.Length);
+float randomPPitch = Random.Rabge(lowPitchRange, highPitchRange);
+sfxSource
+
+        sfxSource.clip = clips[randomIndex];
+        sfxSource.Play();
+    }
+   
 }
